9241b8d21fe856af2686c3e276aa2f62261f8ee8
Damian <AdrianAndradeChristian@outlook.com>
2023-02-21 11:14:50 -0500

    Adding the database

---
+++ BakeryApp/WebClient/Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebClient.Models;
+
+namespace WebClient.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite(@"Data source = bakery.db");
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
