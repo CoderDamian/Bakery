ceaee126cbb8e5756a1feb5453586faf16148a00
Damian <AdrianAndradeChristian@outlook.com>
2023-02-24 16:00:18 -0500

    Restore folders

---
+++ BakeryApp/DataPersistence/BakeryDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DomainModel;
+using Persistence.Mappings;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Persistence
+{
+    public class BakeryDbContext : DbContext
+    {
+        public BakeryDbContext(DbContextOptions<BakeryDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                OracleConfiguration.TnsAdmin = @"C:\Users\Fmla\Documents\OracleWallet\MyERP\";
+                OracleConfiguration.WalletLocation = OracleConfiguration.TnsAdmin;
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfiguration(new ProductMap());
+    }
+}
---
+++ BakeryApp/DomainModel/Seedwork/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace DomainModel.Seedwork
+{
+    public abstract class BaseEntity
+    {
+        public int ID { get; set; }
+    }
+}
